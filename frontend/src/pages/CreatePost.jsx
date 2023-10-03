d43f78af8bd454f2dbbe530983b2bec8ac2f34c1
Viraj Chavan <virajchavan308@gmail.com>
2023-10-03 19:13:40 +0530

    changed to server from localhost

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:4000/api/v1/dalle",{ 
+        const response = await fetch("https://dall-e-wu1u.onrender.com/api/v1/dalle",{ 
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -47,7 +47,7 @@ const handleSubmit = async (e) => {
   if (form.prompt) {
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:4000/api/v1/posts', {
+      const response = await fetch('https://dall-e-wu1u.onrender.com/api/v1/posts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       setLoading(true);
   
       try {
-        const response = await fetch('http://localhost:4000/api/v1/posts', {
+        const response = await fetch('https://dall-e-wu1u.onrender.com/api/v1/posts', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
